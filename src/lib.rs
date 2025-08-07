60d4964d37d841bf0b491a99ea8abcfd537de802
uraneko <BOOTHAlessandra@tutanota.com>
2025-08-07 22:14:14 +0100

    refactor this macro to use new macro_utils api

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 use proc_macro::TokenStream;
-use proc_macro2::Span;
-use proc_macro2::TokenStream as TS2;
-
+use quote::quote;
 use syn::parse::{Parse, ParseStream, Result as PRes};
 use syn::parse_macro_input;
 use syn::{Attribute, Expr, Ident, ItemFn, Lit, LitStr, Token};
 
-use quote::quote;
-
-use pheasant_macro_utils::generate_service;
-use pheasant_macro_utils::{StrAttr, cors, mime, re};
+use pheasant_macro_utils::Method;
+use pheasant_macro_utils::{Plumber, Poet, ServiceInscriptions};
 
 // TODO add a new attribute; CORS, e.g.,
 // #[CORS(methods = [get, post, options], origin = "*", credentials = false, headers = ["X-PingOther", "Content-Type"])]
@@ -19,16 +15,10 @@ pub fn get(attr: TokenStream, fun: TokenStream) -> TokenStream {
     // let [attr, fun]: [TS2; 2] = [attr.into(), fun.into()];
     // println!("{}\n{}", quote! {#attr}, quote! { #fun  });
 
-    let resou = parse_macro_input!(attr as StrAttr);
-    let mut fun = parse_macro_input!(fun as ItemFn);
-    let mime = mime(&mut fun);
-    let re = re(&mut fun);
-    let cors = cors(&mut fun);
+    let plumber = Plumber::new(Method::Get, attr, fun).unwrap();
+    println!("{:?}", plumber);
 
-    let funs = generate_service(resou.into_string(), mime, re, cors, fun);
+    let mut poet = Poet::new(plumber);
 
-    quote! {
-        #funs
-    }
-    .into()
+    poet.assemble().into()
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ struct Params;
 
 #[get("/")]
 #[cors(methods = [get, post], headers = ["Content-Type", "Origin", "Host"], origins = "*", max_age = 213124)] // <- Options preflight req
-#[re("/index.html")] // <- redirection 303 see other
+#[re("/index.html")] // <- redirection 303 see other to this service
 async fn abc(p: Params) -> Response {
     Response
 }
