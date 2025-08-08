f92ba6d9c6d01a1342c8d488c5920a4ba2294dc9
uraneko <BOOTHAlessandra@tutanota.com>
2025-08-08 17:29:05 +0100

    update macro to use the new macro_utils api - remove unused imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,12 @@
+use pheasant_macro_utils::{
+    Method, Plumber, Poet, ServiceInscriptions, ServicePlumber, ServicePoet,
+};
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::parse::{Parse, ParseStream, Result as PRes};
-use syn::parse_macro_input;
-use syn::{Attribute, Expr, Ident, ItemFn, Lit, LitStr, Token};
-
-use pheasant_macro_utils::Method;
-use pheasant_macro_utils::{Plumber, Poet, ServiceInscriptions};
-
-// TODO add a new attribute; CORS, e.g.,
-// #[CORS(methods = [get, post, options], origin = "*", credentials = false, headers = ["X-PingOther", "Content-Type"])]
 
 #[proc_macro_attribute]
 pub fn get(attr: TokenStream, fun: TokenStream) -> TokenStream {
-    // let [attr, fun]: [TS2; 2] = [attr.into(), fun.into()];
-    // println!("{}\n{}", quote! {#attr}, quote! { #fun  });
-
-    let plumber = Plumber::new(Method::Get, attr, fun).unwrap();
-    println!("{:?}", plumber);
-
-    let mut poet = Poet::new(plumber);
+    let plumber = Plumber::<ServicePlumber>::new(Method::Get, attr, fun).unwrap();
+    let mut poet = Poet::<ServicePoet>::new(plumber);
 
     poet.assemble().into()
 }
